f37a8ce3720660f794e1da2a104776beba0cfd73
bruh-9000 <bruh9000games@gmail.com>
2024-03-29 15:27:07 -0700

    Revert always open flyout

--- src/index.js
+++ src/index.js
@@ -436,7 +436,6 @@ document.getElementById('outputCheckbox').addEventListener('change', function(ev
 document.addEventListener("DOMContentLoaded", function() {
   outputPane.style.display = "none";
   Blockly.svgResize(ws);
-  ws.getFlyout().setAutoClose(false);
 });
 
 copyButton.addEventListener("click", () => {
